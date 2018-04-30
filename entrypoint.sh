f626aaaf8836b12f46344f3c41530b7b50234bab
Guillaume POITTEVIN <gpo@atolcd.com>
2018-04-30 11:03:46 +0200

    Fix su-exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 uid=$(stat -c %u /puppet)
 gid=$(stat -c %g /puppet)
 
-sed -r "s/foo:x:\d+:\d+:/foo:x:$uid:$gid:/g" /etc/passwd > /etc/passwd
-sed -r "s/bar:x:\d+:/bar:x:$gid:/g" /etc/group > /etc/group
+sed -i -r "s/foo:x:\d+:\d+:/foo:x:$uid:$gid:/g" /etc/passwd
+sed -i -r "s/bar:x:\d+:/bar:x:$gid:/g" /etc/group
 
 su-exec foo:bar $@
